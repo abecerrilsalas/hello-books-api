50986427fae7da9304fa908a26a3ddd2c733f4d9
Alma <abecerrilsalas@gmail.com>
2021-10-16 22:26:37 -0700

    two initial endpoints

--- app/__init__.py
+++ app/__init__.py
@@ -4,4 +4,8 @@ from flask import Flask
 def create_app(test_config=None):
     app = Flask(__name__)
 
+    from .routes import hello_world_bp #hey flask, from this file, go ahead and imort this variable
+    app.register_blueprint(hello_world_bp)#this registers that variable with flask, so that it knows to use this blueprint for our roots.
+
     return app
+    
--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,19 @@
 from flask import Blueprint
 
+hello_world_bp = Blueprint("hello_world", __name__)#1"hello_world_bp" is our local variable thats going to store a blueprint instance
+#2 "Blueprint" is us creating a blueprint instance. Blueprint is the name of the class
+#3"hello_world" is basically going to be some identifier or name for the blueprint
+#4"__name__" is just what you use universally 
+
+@hello_world_bp.route('/hello-world', methods=["GET"])#using a blueprint decorator. "@" is the decorator ".route" given is '/hello-world'. "methods" is for defining the method you want to design, which is "GET"
+def get_hello_world():
+    my_response="Hello, World!"
+    return my_response
+
+@hello_world_bp.route('/hello-world/JSON', methods=["GET"])
+def hello_world_json():
+    return {
+        "name": "Alma",
+        "message": "sup",
+        "hobbies": ["reading", "laughing", "listen to music"],
+    }, 200#"200" is the status code 
